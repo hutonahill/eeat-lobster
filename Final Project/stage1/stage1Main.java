d3c20d18aaef765d787422152039d9e8e68da463
hutonahill <35947141+hutonahill@users.noreply.github.com>
2022-10-07 17:21:03 -0600

    crated main files for gui and implimenter

---
+++ Final Project/stage1/stage1Main.java
@@ -0,0 +1,7 @@
+
+public class stage1Main {
+    public static public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Final Project/stage1/welcomeFrame.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class welcomeFrame extends JFrame{
+    
+}
