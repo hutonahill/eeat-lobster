129dbeb33fdade1ce94a47da0fc28b6f080844dd
Aaron  Jones <jon17049@byui.edu>
2022-10-08 10:51:08 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

---
+++ Final Project/stage2/Processor/dataParser.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+file = pd.read_csv(
+    '/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv')
+
+# Prints the column information for the specified column name.
+column_name = "Margin (Sale)"
+print(file.filter(regex=column_name))
+
+# file.describe()
+
+# Iterating the columns and displaying the column headers
+# for col in file.columns:
+#     print(col)
+
+# Another method to display the column headers
+# list(data) or
+# list(data.columns)
+
+
+# file["Profit"]
+# print(file)
--- Final Project/stage2/Processor/solution.java
+++ Final Project/stage2/Processor/solution.java
@@ -6,12 +6,13 @@ import java.nio.charset.StandardCharsets;
 public class solution {
 
     public static void main(String[] args) {
-        
-        runPython(".\\test1.py");
+
+        runPython("test1.py");
 
     }
 
     public static Process mProcess;
+
     public static void runPython(String fileLocation) {
         Process process;
         try {
@@ -20,7 +21,7 @@ public class solution {
         } catch (Exception e) {
             System.out.println("Exception Raised" + e.toString());
         }
-        InputStream stdout = (mProcess != null) ? mProcess.getInputStream(): null;
+        InputStream stdout = (mProcess != null) ? mProcess.getInputStream() : null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
         String line;
         try {
