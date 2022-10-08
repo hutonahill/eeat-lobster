fca667495950a6f2b1dbd8f293909688d2f46ce8
Aaron  Jones <jon17049@byui.edu>
2022-10-07 18:34:21 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

---
+++ Final Project/stage2/Processor/testProcessor.java
@@ -0,0 +1,28 @@
+// package FinalProject.stage2.Processor;
+
+import java.io.*;
+
+public class testProcessor {
+    public static void main(String args[]) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("input.txt");
+            out = new FileOutputStream("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+}
