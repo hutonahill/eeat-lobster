06aba1b70ac9bd498ccd4aeedce7757850eadd3c
Aaron  Jones <jon17049@byui.edu>
2022-10-08 13:01:01 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

--- Final_Project/stage2/Processor/dataParser.py
+++ Final_Project/stage2/Processor/dataParser.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class Parser:
     def __init__(self) -> None:
         pass
@@ -12,7 +13,7 @@ file = pd.read_csv(
     '/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv')
 
 # Prints the column information for the specified column name.
-column_name = "Margin (Sale)"
+column_name = "Profit"
 print(file.filter(regex=column_name))
 
 # file.describe()
