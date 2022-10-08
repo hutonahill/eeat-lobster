ad76c4bdd8deb81fcfe469e4ccdc157858f81611
Blazelighter <104170619+Blazelighter@users.noreply.github.com>
2022-10-08 15:59:57 -0600

    attempt to filter

--- Final_Project/stage2/Processor/Seeker.py
+++ Final_Project/stage2/Processor/Seeker.py
@@ -4,31 +4,32 @@ class Seeker:
     def __init__(self) -> None:
         pass
 
-    def Seek(self, filepath, sortBy, filter1):
+    def Seek(filepath, sortBy, filterList):
         file = pd.read_csv(filepath)
 
-        if sortBy in file.columns():
-
-            pass
-
-        filterList = "011profit"
+        filterList = "001Profit"
 
         
 
-        if filterList[0]:
+        if filterList[0] == 1:
             file.filter(regex="")
-        if filterList[1]:
+        if filterList[1] == 1:
             file.filter(regex="")
-        if filter1:
+        if filterList[2] == 1:
             file.filter(like="$0.00")
 
         file.filter(regex = sortBy)
-        file.sort_values()
+        file.sort_values(sortBy)
+
+        pd.display(file)
 
         # if filterList[0]:
         #     pass
 
 
+sk = Seeker
+sk.Seek("C:\\Users\\Todd\Downloads\\Slead Shead data(1)\\Slead Shead data\\Sales 7-1 to 9-30.csv", 
+"Profit", "001Profit")
 
 
 # file = pd.read_csv(
