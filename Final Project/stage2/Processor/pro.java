4a1eb33788ae2273967006c5c259e1cdf63c5f4f
Aaron  Jones <jon17049@byui.edu>
2022-10-07 21:30:35 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

---
+++ Final Project/stage2/Processor/pro.java
@@ -0,0 +1,45 @@
+// package FinalProject.stage2.Processor;
+
+// import java.io.*;
+// import java.util.Scanner;
+
+// public class testProcessor {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(new File("/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/book.csv"));
+//         // parsing a CSV file into the constructor of Scanner class
+//         sc.useDelimiter(",");
+//         // setting comma as delimiter pattern
+//         while (sc.hasNext()) {
+//             System.out.print(sc.next());
+//         }
+//         sc.close();
+//         // closes the scanner
+//     }
+// }
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class pro {
+    public Process mProcess;
+
+    public pro() {
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(new String[] { "test1", "arg1", "arg2" });
+            mProcess = process;
+        } catch (Exception e) {
+            System.out.println("Exception Raised" + e.toString());
+        }
+        InputStream stdout = mProcess.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println("stdout: " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception in reading output" + e.toString());
+        }
+    }
+}
---
+++ Final Project/stage2/Processor/solution.java
@@ -0,0 +1,9 @@
+
+// package FinalProject.stage2.Processor;
+
+public class solution {
+    public static void main(String[] args) {
+        pro test1 = new pro();
+        // test1.runScript();
+    }
+}
