8955fd87b347cd3907b8006aaaad8a21f306b9bc
echris10sen <chr15016@byui.edu>
2022-10-07 22:56:24 -0600

    next bit

--- Final Project/stage2/Processor/pythonRunner.java
+++ Final Project/stage2/Processor/pythonRunner.java
@@ -6,11 +6,12 @@ import java.lang.Object;
 
 public class pythonRunner {
     public void runPythonScript() throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder("python", resolvePythonScriptPath("hello.py"));
-        processBuilder.redirectErrorStream(true);
+        // ProcessBuilder processBuilder = new ProcessBuilder("python",
+        // resolvePythonScriptPath("hello.py"));
+        // processBuilder.redirectErrorStream(true);
 
-        Process process = processBuilder.start();
-        List<String> results = readProcessOutput(process.getInputStream());
+        // Process process = processBuilder.start();
+        // List<String> results = readProcessOutput(process.getInputStream());
 
         // assertThat("Results should not be empty", results, is(not(empty())));
         // assertThat("Results should contain output of script: ", results, hasItem(
