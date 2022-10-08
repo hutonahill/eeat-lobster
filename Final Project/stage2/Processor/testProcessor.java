8fd7ef9642c60b60e2d37e5a1f8d1df6d9520004
Aaron  Jones <jon17049@byui.edu>
2022-10-07 19:10:14 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

--- Final Project/stage2/Processor/testProcessor.java
+++ Final Project/stage2/Processor/testProcessor.java
@@ -1,28 +1,18 @@
 // package FinalProject.stage2.Processor;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class testProcessor {
-    public static void main(String[] args) throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
-
-        try {
-            in = new FileInputStream("/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv");
-            out = new FileOutputStream("/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile1.csv");
-
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write(c);
-            }
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv"));
+        // parsing a CSV file into the constructor of Scanner class
+        sc.useDelimiter(",");
+        // setting comma as delimiter pattern
+        while (sc.hasNext()) {
+            System.out.print(sc.next());
         }
+        sc.close();
+        // closes the scanner
     }
-
 }
