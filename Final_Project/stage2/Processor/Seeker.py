006420c13465ab1400e3039222e22ffeb88cfe1f
Blazelighter <104170619+Blazelighter@users.noreply.github.com>
2022-10-08 13:36:42 -0600

    created Seeker class

--- Final_Project/stage2/Processor/Seeker.py
+++ Final_Project/stage2/Processor/Seeker.py
@@ -1,20 +1,24 @@
 import pandas as pd
 
-
-class Parser:
+class Seeker:
     def __init__(self) -> None:
         pass
 
-    def Parse(filepath):
-        pass
+    def Seek(filepath):
+        file = pd.read_csv(filepath)
+
+        # if filterList[0]:
+        #     pass
 
 
-file = pd.read_csv(
-    '/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv')
+
+
+# file = pd.read_csv(
+#     '/Users/jones/Desktop/Hack-A-Thon/Slead Shead data 2/salesTestFile.csv')
 
 # Prints the column information for the specified column name.
-column_name = "Profit"
-print(file.filter(regex=column_name))
+# column_name = "Profit"
+# print(file.filter(regex=column_name))
 
 # file.describe()
 
